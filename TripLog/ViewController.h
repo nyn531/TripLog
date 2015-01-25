235fd428c23804717c7f0783fba416a1dc02c675
Yinan Na <nyn531@gmail.com>
2015-01-24 17:00:21 -0800

    Initial Commit

---
+++ TripLog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TripLog
+//
+//  Created by Yinan Na on 1/24/15.
+//  Copyright (c) 2015 com.lyft.ynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TripLog/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TripLog
+//
+//  Created by Yinan Na on 1/24/15.
+//  Copyright (c) 2015 com.lyft.ynn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
